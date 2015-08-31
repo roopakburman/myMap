2f1c8316c4aae85ac24bcb15d28410806502e685
roopakburman <roopak.burman@gmail.com>
2015-08-31 11:03:01 -0400

    first commit

---
+++ www/js/app.js
@@ -0,0 +1,37 @@
+angular.module('starter', ['ionic'])
+.controller('myMapCtrl', function($scope, $ionicLoading){
+	google.maps.event.addDomListener(window, 'load', function() {
+        var myLatlng = new google.maps.LatLng(38.897670, -77.070519);
+ 
+        var mapOptions = {
+            center: myLatlng,
+            zoom: 16,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+ 
+        var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+ 
+        navigator.geolocation.getCurrentPosition(function(pos) {
+            map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+            var myLocation = new google.maps.Marker({
+                position: new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude),
+                map: map,
+                title: "My Location"
+            });
+        });
+ 
+        $scope.map = map;
+    });
+})
+
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
